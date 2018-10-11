5309ee6a789facef971788e933ea946fe5b1b857
Cheyanna <cheyanna.graham@gmail.com>
2018-10-10 21:56:26 -0700

    Map Loaded!

--- src/App.js
+++ src/App.js
@@ -2,15 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 import Map from './Components/Map'
 import ListView from './Components/ListView'
+// import * from 
 
 
 class App extends Component {
+  
   constructor(props) {
     super(props);
     this.state = 
     {
       markers : '732 stonebridge way, pleasant hill, ca'
     }
+  }
+
+  componentDidMount() {
+    this.getMap();
+  }
+
+  getMap() {
+    // let map = new google.maps.Map(
+    //   document.createElement('div'),
+    //   {
+    //     center:
+    //     {
+    //         lat:41.159577,
+    //         lng:-74.255406
+    //     },
+    //     zoom:9
+    //   }
+    // )
   }
 
   render() {
--- src/index.js
+++ src/index.js
@@ -4,7 +4,32 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// let map;
+
+window.initMap = function() {
+    let map,mapError;
+
+    //Check if library loaded
+    if(window.google.maps)
+    {
+        map = new window.google.maps.Map(
+        document.querySelector('#map'),
+        {
+            center : {
+                lat:41.263849,
+                lng:-74.382206
+            },
+            zoom : 11
+        })
+
+    } else {
+        mapError = "Error, Google Maps Not Loaded";
+    } 
+
+    ReactDOM.render(<App map = {map} mapError = {mapError}/>, document.getElementById('root'));
+
+}
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
