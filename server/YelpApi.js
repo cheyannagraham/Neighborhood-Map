fe41e1397329fb719f42f5d6c5722d693c65b301
Cheyanna <cheyanna.graham@gmail.com>
2018-10-20 13:08:35 -0700

    build marker object in api

--- server/YelpApi.js
+++ server/YelpApi.js
@@ -11,24 +11,41 @@ app.use(bodyParser.json());
 
 
 app.post('/', (req, res) => {
-    console.log(req.body)
-    request({
-        url : `https://api.yelp.com/v3/businesses/search?term=${req.body.keyword}&location=${req.body.location}&radius=20000`,
-        method: 'GET',
-        'auth' : {
-            'bearer' : '3Yd189W5j3gAG3VCG6L1mDurEi8fwJ340aLNzNR-gjfn__bvCPmFH3UOkuEr5Tq_Z0svny-KQcCfQL_eXWDzK327bfEOzX8g67Bgtuol8HTVPbbbBf5ebqMlksvGW3Yx'
-        }
-    },
-    (error,response,body) => {
-        error && res.send(error);
+	console.log(req.body)
+	request({
+		url : `https://api.yelp.com/v3/businesses/search?term=${req.body.keyword}&location=${req.body.location}&radius=20000`,
+		method: 'GET',
+		'auth' : {
+			'bearer' : '3Yd189W5j3gAG3VCG6L1mDurEi8fwJ340aLNzNR-gjfn__bvCPmFH3UOkuEr5Tq_Z0svny-KQcCfQL_eXWDzK327bfEOzX8g67Bgtuol8HTVPbbbBf5ebqMlksvGW3Yx'
+		}
+	},
+	(error,response,body) => {
+		error && res.send(error);
+		console.log(response.statusCode);   
 
-        console.log(response.statusCode);        
-
-        JSON.parse(body).businesses ? 
-        res.send(JSON.parse(body).businesses.filter((bus,index) => index < 5 )) : 
-        res.send([]);   
-
-    }); 
+		let results = JSON.parse(body).businesses ? 
+		JSON.parse(body).businesses.filter((bus,index) => index < 5 ) : 
+		[];
+				
+		let markers = results.map(bus =>
+			({
+				id : bus.id,
+				position : {
+					lat : bus.coordinates.latitude,
+					lng : bus.coordinates.longitude
+				},
+				address : bus.location.display_address.join(' '),
+				title : bus.name,
+				rating : bus.rating,
+				price : bus.price,
+				reviewCount : bus.review_count,
+				avatar : bus.image_url,
+				phone : bus.display_phone,
+				website : bus.url
+			})
+		)
+		res.send(markers)
+	}); 
 }) 
 
 
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -2,48 +2,24 @@ import React from 'react'
 
 class Search extends React.Component {
 
-    handleChange = (input,field) => {
-       this.setState({[field] : input})
-       
-       fetch('http://localhost:3002',
-       {
-           method: 'POST',
-           headers: {
-               'content-type' : 'application/json'
-           },
-           body : JSON.stringify({
-               keyword: field === 'keyword' ? input : (this.state && this.state.keyword) || 
-               'coffee',
-               location: field === 'location' ? input : (this.state && this.state.location )|| 'walnut creek,ca'
-           })
-       })
-       .then(resp => resp.text())
-       .then(resp => {
-
-            if(resp){
-
-                let markers = JSON.parse(resp).map(bus =>
-                    ({
-                        id : bus.id,
-                        position : {
-                            lat : bus.coordinates.latitude,
-                            lng : bus.coordinates.longitude
-                        },
-                        address : bus.location.display_address.join(' '),
-                        title : bus.name,
-                        rating : bus.rating,
-                        price : bus.price,
-                        reviewCount : bus.review_count,
-                        avatar : bus.image_url,
-                        phone : bus.display_phone,
-                        website : bus.url
-
-                    })
-                )        
-                this.props.getMarkers(markers)                
-            }             
-        });
-    }
+  handleChange = (input,field) => {
+    this.setState({[field] : input})
+      
+    fetch('http://localhost:3002',
+    {
+      method: 'POST',
+      headers: {
+        'content-type' : 'application/json'
+      },
+      body : JSON.stringify({
+        keyword: field === 'keyword' ? input : (this.state && this.state.keyword) || 
+        'coffee',
+        location: field === 'location' ? input : (this.state && this.state.location )|| 'walnut creek,ca'
+      })
+    })
+    .then(resp => resp.json())
+    .then(resp => this.props.getMarkers(resp));
+}
 
 
     render(){
