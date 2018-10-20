6226aa5d70e85ccd80fe05dc8bed991a8a410ad9
Cheyanna <cheyanna.graham@gmail.com>
2018-10-19 19:51:56 -0700

    show mod with data onclick

--- src/Components/ListView.js
+++ src/Components/ListView.js
@@ -15,10 +15,25 @@ class ListView extends React.Component {
                 
                 <ol>
                     {this.props.markers.map(marker => {
-                        let className = '';
-                        
-                        this.props.markerClicked && marker.title === this.props.markerClicked.title ?
-                        className = 'clicked' : className = '';
+                        let className, display;
+
+                        //toggle clicked class if marker clicked
+                        if(this.props.markerClicked && marker.title === this.props.markerClicked.title){
+                            className = 'clicked';
+                            display = 
+                            <div>
+                                <ol id='module-list'>
+                                    <li>Address: {marker.address}</li>
+                                    <li>Phone: {marker.phone}</li>
+                                    <li>Website: {marker.website}</li>
+                                </ol>
+                            </div>
+                        } 
+                        else{
+                            className = '';
+                            display = '';
+
+                        }
 
                         return (
                             <li  
@@ -28,6 +43,7 @@ class ListView extends React.Component {
                                 onClick = {() => {this.props.handleClick(marker)}}>
                                     {marker.title} 
                                 </a>
+                                {display}
                             </li>
                         )                        
                     })}
