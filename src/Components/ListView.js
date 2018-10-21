d77d26cb07799f5a7722eb6c1c42882e7485766a
Cheyanna <cheyanna.graham@gmail.com>
2018-10-20 21:38:02 -0700

    factor out make list component

--- src/Components/ListView.js
+++ src/Components/ListView.js
@@ -9,65 +9,66 @@ class ListView extends React.Component {
   }
 
   filter = () => {
-    return <input 
-            type='text' 
-            value = {this.searchField} 
-            onChange = { (e) => {this.props.findResults(e.target.value)}} />
+    return (
+    <input 
+      type='text' 
+      value = {this.searchField} 
+      onChange = { (e) => {this.props.findResults(e.target.value)}} />
+    )
   }
 
   modDisplay = (marker) => {
-    return <div>
-              <ol id='module-list'>
-                <li id='street-view'></li>
-                <li>{marker.address}</li>
-                <li>{marker.phone}</li>
-                <li><a href={marker.website}>Visit</a></li>
-              </ol>
-            </div>
+    return (
+    <div>
+      <ol id='module-list'>
+        <li id='street-view'></li>
+        <li>{marker.address}</li>
+        <li>{marker.phone}</li>
+        <li><a href={marker.website}>Visit</a></li>
+      </ol>
+    </div>
+    )
+  }
+
+  makeList = () => {
+    return (
+      <ol>
+        {this.props.markers.map(marker => {
+          let className = '';
+          let display = '';
+
+          //toggle clicked class if marker clicked
+          if(this.props.markerClicked && marker.title === this.props.markerClicked.title) {            
+            className = 'clicked';							
+            display = this.modDisplay(marker);
+          }
+
+          return (
+            <li                    
+              key = {marker.id}>
+                
+              <button 
+                className = {className}
+                onClick = {() => {this.props.handleClick(marker)}} >
+                {marker.title} 
+              </button>
+              
+              {display}
+            </li>
+          )                                  
+        })}
+      </ol>
+    ) 
   }
   
 	render() {
 		return (
 			<div>
-      Filter Results {this.filter()}        				
+        Filter Results {this.filter()}        				
 
-				{this.props.markers && this.props.markers.length > 0 ? 
-				
-				<ol>
-					{this.props.markers.map(marker => {
-						let className, display;
+        {this.props.markers && this.props.markers.length > 0 
+        ? this.makeList() : <p>No Results</p>}
 
-						//toggle clicked class if marker clicked
-						if(this.props.markerClicked && marker.title === this.props.markerClicked.title) {
-							
-              className = 'clicked';							
-              display = this.modDisplay(marker);
-
-            }
-            else {
-              className = '';
-              display = '';
-            }
-
-            return (
-              <li  
-              key = {marker.id || marker.title}>
-                  
-                <button 
-                className = {className}
-                onClick = {() => {this.props.handleClick(marker)}} >
-                  {marker.title} 
-                </button>
-                
-                {display}
-              </li>
-            )                        
-							})}
-					</ol> :
-
-					<p>No Results</p>
-
-				}
 			</div>
 		)
 	}
