b5bfed8b6df30a46d56ac7687398f57d4f44153a
Cheyanna <cheyanna.graham@gmail.com>
2018-11-05 22:25:46 -0800

    SW cache on install event

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,43 @@
+self.addEventListener('activate', event  => {
+    event.waitUntil(
+        caches.open('Neighborhood-Map')
+        .then(cache => {
+            cache.addAll([
+                "/",
+                "/static/js/bundle.js",
+                "/static/js/1.chunk.js",
+                "/static/js/main.chunk.js",
+                "/main.a2f314f61455069e6be7.hot-update.js",
+                "http://localhost:3002/search?keyword=coffee&location=NY",
+                "https://maps.gstatic.com/mapfiles/openhand_8_8.cur",
+                "/sockjs-node/info?t=1541485291957",
+                "https://maps.gstatic.com/mapfiles/transparent.png",
+                "https://maps.gstatic.com/mapfiles/api-3/images/spotlight-poi2_hdpi.png",
+                "/manifest.json",
+                "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Google+Sans",
+                "https://maps.gstatic.com/mapfiles/api-3/images/google4_hdpi.png"
+            ])
+        })
+    )
+
+
+
+})
+
+let urls = [];
+
+
 self.addEventListener('fetch',event => {
+    console.log(event.request.url)
+
     if(event.request.url.includes('maps.googleapis.com') || event.request.method !== 'GET') {
         return
     }
 
     else if(event.request.url.includes('http')) {
-        console.log(event.request.url);
+        urls.push(event.request.url);
+        console.log(urls)
         event.respondWith(
-            //cache static files
             caches.match(event.request.url)
             .then(response => {
                 if(response){
