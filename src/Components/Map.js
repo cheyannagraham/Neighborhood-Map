69ebc90e6a77656153ef9830e8fc784cb22e3bb2
Cheyanna <cheyanna.graham@gmail.com>
2018-11-07 18:58:21 -0800

    a11y add aria-labelledby

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -125,35 +125,35 @@ class Map extends React.Component{
   fillInfoWindow = (marker) => {
     let display = (`
       <div tabindex="0" aria-label="info-window" id = "info-window" >
-        <ul aria-label="${marker.title} information">
-          <li>
-            <h3 id="info-window-title" >${marker.title}</h3>
-          </li>
           
-          <li>
-            <image class = "avatar" src = "${marker.avatar}" alt = "${marker.title} image">
-          </li>
-                    
-          <li> 
-              <img class="yelp-rating-logo" 
-              src=${marker.rating && this.getRatingImage(marker.rating)} 
-              alt="yelp rating-logo" />
-                ${marker.rating} (${marker.reviewCount})
-          </li>
+        <h3 id="info-window-title">${marker.title}</h3>
           
-          <li>
-            Price: ${marker.price || ""}
-          </li> 
+          <ul aria-labelledby="info-window-title">
           
-          <li id="open-status">
-            ${marker.hours && marker.hours[0].is_open_now ? "Open now" : "Closed"} 
-          </li>
-          
-          <li>
-            <a id="info-window-logo" href=${marker.website}>
-              <img class="yelp-logo" src = "images/Yelp_trademark_rgb.png" alt="Yelp logo"/>
-            </a>            
-          </li>
+            <li>
+              <image class = "avatar" src = "${marker.avatar}" alt = "${marker.title} image">
+            </li>
+                      
+            <li> 
+                <img class="yelp-rating-logo" 
+                src=${marker.rating && this.getRatingImage(marker.rating)} 
+                alt="yelp rating-logo" />
+                  ${marker.rating} (${marker.reviewCount})
+            </li>
+            
+            <li>
+              Price: ${marker.price || ""}
+            </li> 
+            
+            <li id="open-status">
+              ${marker.hours && marker.hours[0].is_open_now ? "Open now" : "Closed"} 
+            </li>
+            
+            <li>
+              <a id="info-window-logo" href=${marker.website}>
+                <img class="yelp-logo" src = "images/Yelp_trademark_rgb.png" alt="Yelp logo"/>
+              </a>            
+            </li>
         </ul>
         
       </div>`)
